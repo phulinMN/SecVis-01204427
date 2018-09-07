769e8062df13c370bbb05718a8829347a456717c
phulinMN <pongpaiMN@gmail.com>
2018-09-07 11:37:48 +0700

    change 3-2 top most

--- week4/python/pararell.py
+++ week4/python/pararell.py
@@ -23,28 +23,55 @@ for i in range(60):
         r = str(i)
     t_min.append(r)
 # print(t_min)
-username = []
-ip_src = []
-ip_dst = []
-port_dst = []
-host = []
+top_username = []
+username = {}
+ip_src = {}
+ip_dst = {}
+port_dst = {}
+host = {}
+for i in arr_lines:
+    i = i.replace('\n', '')
+    i = i.split(' ')
+    if i[1] not in username:
+        username[i[1]] = 0
+    else:
+        username[i[1]] += 1
+
+sort_user = sorted(username.values(), reverse=True)
+for i in username:
+    for x in range(10):
+        if username[i] == sort_user[x]:
+            top_username.append(i)
+print(top_username)
+
 for i in arr_lines:
     i = i.replace('\n', '')
     i = i.split(' ')
     a = convert_time(i[0])
     a = a.split(':')[2]
-    data.append([a, i[1], i[2], i[3], i[4], i[5]])
-    if i[1] not in username:
-        username.append(i[1])
-    if i[2] not in ip_src:
-        ip_src.append(i[2])
-    if i[3] not in ip_dst:
-        ip_dst.append(i[3])
-    if i[4] not in port_dst:
-        port_dst.append(i[4])
-    if i[5] not in host:
-        host.append(i[5])
-print(host)
+    if i[1] in top_username:
+        if i[2] not in ip_src:
+            ip_src[i[2]] = 0
+        else:
+            ip_src[i[2]] += 1
+        if i[3] not in ip_dst:
+            ip_dst[i[3]] = 0
+        else:
+            ip_dst[i[3]] += 1
+        if i[4] not in port_dst:
+            port_dst[i[4]] = 0
+        else:
+            port_dst[i[4]] += 1
+        if i[5] not in host:
+            host[i[5]] = 0
+        else:
+            host[i[5]] += 1
+        data.append([a, i[1], i[2], i[3], i[4], i[5]])
+print(sorted(ip_src.keys()))
+print(sorted(ip_dst.keys()))
+print(sorted(port_dst.keys()))
+print(sorted(host.keys()))
+print(t_min)
 
 with open('parallel.json', 'w') as outfile:
     json.dump(data, outfile)
