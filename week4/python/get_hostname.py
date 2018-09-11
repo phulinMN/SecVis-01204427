bb0f8fb1c9c4f69bb8885e1284f359e2e07e10c5
phulinMN <pongpaiMN@gmail.com>
2018-09-11 15:10:31 +0700

    chage data

--- week4/python/get_hostname.py
+++ week4/python/get_hostname.py
@@ -4,34 +4,35 @@ arr_lines = []
 arr_lines2 = []
 with open('egress_hostname.txt') as f:
     arr_lines = f.readlines()
-with open('ingress_hostname.txt') as f:
-    arr_lines2 = f.readlines()
+# with open('ingress_hostname.txt') as f:
+#     arr_lines2 = f.readlines()
 data = []
-data2 = []
+# data2 = []
 
 for i in arr_lines:
     i = i.replace('\n', '')
     x = i.split(' ')
-    x = x[-3:]
+    x = x[-2:]
+    print(x)
     child = {
-        'value': x[0],
-        'name': x[2]
+        'value': int(x[0]),
+        'name': x[1]
     }
     data.append(child)
 print(data)
 
-for i in arr_lines2:
-    i = i.replace('\n', '')
-    x = i.split(' ')
-    x = x[-3:]
-    child = {
-        'value': x[0],
-        'name': x[2]
-    }
-    data2.append(child)
-print(data2)
+# for i in arr_lines2:
+#     i = i.replace('\n', '')
+#     x = i.split(' ')
+#     x = x[-3:]
+#     child = {
+#         'value': x[0],
+#         'name': x[2]
+#     }
+#     data2.append(child)
+# print(data2)
     
-with open('egress_hostname.json', 'w') as outfile:
+with open('e_hostname.json', 'w') as outfile:
     json.dump(data, outfile)
-with open('ingress_hostname.json', 'w') as outfile:
-    json.dump(data2, outfile)
+# with open('ingress_hostname.json', 'w') as outfile:
+#     json.dump(data2, outfile)
--- week4/python/user.py
+++ week4/python/user.py
@@ -5,15 +5,23 @@ with open('user.txt') as f:
     arr_lines = f.readlines()
 
 data = []
+list_data = {}
 for i in arr_lines:
     i = i.replace('\n', '')
     x = i.split(' ')
     x = x[-2:]
+    list_data[x[1]] = int(x[0])
+sort_user = sorted(list_data, key=list_data.__getitem__, reverse=True)
+sort_value = sorted(list_data.values(), reverse=True)
+
+for i in range(len(sort_user)):
+    print(i)
     child = {
-        'value': x[0],
-        'name': x[1]
+        'value': sort_value[i],
+        'name': sort_user[i]
     }
     data.append(child)
+
 print(data)
 
 with open('user_request.json', 'w') as outfile:
